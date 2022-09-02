c064e600b158b32b81826e2e447481643b90d888
OHSESOL <tpthf1@gmail.com>
2022-09-02 15:08:01 +0900

    js2

--- js/reviewList.js
+++ js/reviewList.js
@@ -1,3 +1,4 @@
+//idx에 맞는 영상 띄우는 로직
 const url = new URL(window.location.href);
 const urlParam = url.searchParams;
 
@@ -7,3 +8,14 @@ let jsonData =  localStorage.getItem(`video${idx}`);
 let video = JSON.parse(jsonData);
 let frame = document.querySelector("iframe");
 frame.setAttribute("src", video.url);
+
+
+//리뷰 작성 버튼 관련 로직
+const makeBtn = document.querySelector("#makeReview");
+makeBtn.addEventListener("click", ()=>{
+  window.open("makeReview.html", "poll", "width=600, height=550, top=150, left=200");
+});
+
+//리뷰 받아와서 띄우는 로직
+const reviews = localStorage.getItem(`review${idx}`);
+let reviewList = "";
