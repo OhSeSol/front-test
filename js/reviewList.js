fda802bae918a8d31ffb97dc6d7deb269e9c2b49
김태균 <taekyun.contact@gmail.com>
2022-09-02 15:11:36 +0900

    dumy

--- js/reviewList.js
+++ js/reviewList.js
@@ -1,5 +1,9 @@
-//url 부터 읽어야함..
-
-const url = new URL(window.location.search);
+const url = new URL(window.location.href);
 const urlParam = url.searchParams;
 
+let idx = urlParam.get('idx');
+
+let jsonData =  localStorage.getItem(`video${idx}`);
+let video = JSON.parse(jsonData);
+let frame = document.querySelector("iframe");
+frame.setAttribute("src", video.url);
